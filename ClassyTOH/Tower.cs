2c7f6ed402d36a5c6ad6fa41b59ae62829589259
Leila Schmidt <95034223+LeilaSchmidt@users.noreply.github.com>
2023-03-27 16:07:23 +0200

    Add project files.

---
+++ ClassyTOH/Tower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyTOH
+{
+    class Tower
+    {
+
+    }
+
+    class TowerA : Tower
+    {
+
+    }
+
+    class TowerB : Tower
+    {
+
+    }
+
+    class TowerC : Tower
+    {
+
+    }
+}
---
+++ ClassyTOH/Towers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyTOH
+{
+    class Towers
+    {
+        public void Movement()
+        {
+            Console.WriteLine("This class represents the movement between towers.");
+        }
+    }
+}
